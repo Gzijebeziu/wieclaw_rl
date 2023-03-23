bb61932cfdb9270a444c488d93b285e25acb34c1
Gzijebeziu <thejakubartur@gmail.com>
2023-03-23 15:38:04 +0100

    monster spawner

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{GameState, RGB, Rltk, RandomNumberGenerator, Point};
+use rltk::{GameState, Rltk, Point};
 use specs::prelude::*;
 mod components;
 pub use components::*;
@@ -20,6 +20,7 @@ mod damage_system;
 use damage_system::DamageSystem;
 mod gui;
 mod gamelog;
+mod spawner;
 
 
 #[derive(PartialEq, Copy, Clone)]
@@ -127,47 +128,12 @@ fn main() -> rltk::BError {
     let (player_x, player_y) = map.rooms[0].center();
 
 
-    let player_entity = gs.ecs
-        .create_entity()
-        .with(Position { x: player_x, y: player_y })
-        .with(Renderable {
-            glyph: rltk::to_cp437('☺'),
-            fg: RGB::from_u8(255, 201, 14),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(Player{})
-        .with(Viewshed{ visible_tiles : Vec::new(), range : 8, dirty : true })
-        .with(Name{ name: "Wieclaw".to_string() })
-        .with(CombatStats{ max_hp: 30, hp: 30, defense: 2, power: 5 })
-        .build();
+    let player_entity = spawner::player(&mut gs.ecs, player_x, player_y);
+    gs.ecs.insert(rltk::RandomNumberGenerator::new());
+    for room in map.rooms.iter().skip(1) {
+        spawner::spawn_room(&mut gs.ecs, room);
+    }
 
-
-    let mut rng = RandomNumberGenerator::new();
-    for (i,room) in map.rooms.iter().skip(1).enumerate() {
-        let (x,y) = room.center();
-
-        let glyph : rltk::FontCharType;
-        let name : String;
-        let roll = rng.roll_dice(1, 2);
-        match roll {
-            1 => { glyph = rltk::to_cp437('g'); name = "Gnomon".to_string(); }
-            _ => { glyph = rltk::to_cp437('G'); name = "Golem Zoledny".to_string(); }
-        }
-
-        gs.ecs.create_entity()
-            .with(Position{ x, y })
-            .with(Renderable{
-                glyph: glyph,
-                fg: RGB::named(rltk::GREEN3),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(Viewshed{ visible_tiles: Vec::new(), range: 8, dirty: true })
-            .with(Monster{})
-            .with(Name{ name: format!("{} #{}", &name, i) })
-            .with(BlocksTile{})
-            .with(CombatStats{ max_hp: 16, hp: 16, defense: 1, power: 4 })
-            .build();
-     }
 
     gs.ecs.insert(gamelog::GameLog{ entries : vec!["Wieclaw wpadl do jakiejs dziury w ziemi czy cos i zgubil swoj Zloty Zombek!".to_string()]});
     gs.ecs.insert(RunState::PreRun);
